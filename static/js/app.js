a1790591c31bf022f6ef68507e315dd2278391f4
daneshtavana <danesh@gmail.com>
2020-07-24 14:30:33 -0700

    section 11.5.4

--- static/js/app.js
+++ static/js/app.js
@@ -3,13 +3,13 @@ const tableData = data;
 // Reference the HTML table using d3
 var tbody = d3.select("tbody");
 //
-//Section 11.5.1 Introductin to Dynamic Tables
+// Section 11.5.1 Introduction to Dynamic Tables
 //
 function buildTable(data) {
     // First, clear out any existing data
     tbody.html("");
 
-    //Section 11.5.2 Add for Each to your Table
+    // Section 11.5.2 Add for Each to your Table
     // Next, loop through each object in the data
     // and append a row and cells for each value in the row
     data.forEach((dataRow) => {
@@ -25,3 +25,32 @@ function buildTable(data) {
       );
     });
 }
+//
+// Section 11.5.3 Add Filters
+//
+function handleClick() {
+    // Grab the datetime value from the filter
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    
+    // Check to see if a date was entered and filter the
+    // data using that date.
+    // Section 11.5.4 Use the "If" Statement
+    if (date) {
+      // Apply `filter` to the table data to only keep the
+      // rows where the `datetime` value matches the filter value
+      filteredData = filteredData.filter(row => row.datetime === date);
+    };
+    
+    // Rebuild the table using the filtered data
+    // @NOTE: If no date was entered, then filteredData will
+    // just be the original tableData.
+    buildTable(filteredData);
+  };
+  
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the table when the page loads
+buildTable(tableData);
+
